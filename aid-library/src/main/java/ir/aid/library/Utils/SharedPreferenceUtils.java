aeb9d96c040fd003dc610de0a2c87dfd60491f81
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-04 13:27:21 +0330

    commit init
    commit init

--- aid-library/src/main/java/ir/aid/library/Utils/SharedPreferenceUtils.java
+++ aid-library/src/main/java/ir/aid/library/Utils/SharedPreferenceUtils.java
@@ -8,28 +8,44 @@ public class SharedPreferenceUtils {
     private static String DEVELOPER = "محمد علی ریاضتی";
 
     private static SharedPreferences preference(Context context, String spFileName) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(spFileName, Context.MODE_PRIVATE);
-        return sharedPreferences;
+        return context.getSharedPreferences(spFileName, Context.MODE_PRIVATE);
     }
 
     public static String readString(Context context, String spFileName, String key, String alternative) {
-        final String str = preference(context, spFileName).getString(key, alternative);
-        return str;
+        return preference(context, spFileName).getString(key, alternative);
+    }
+
+    public static float readFloat(Context context, String spFileName, String key, float alternative) {
+        return preference(context, spFileName).getFloat(key, alternative);
+    }
+
+    public static long readLong(Context context, String spFileName, String key, long alternative) {
+        return preference(context, spFileName).getLong(key, alternative);
     }
 
     public static int readInteger(Context context, String spFileName, String key, int alternative) {
-        final int integer = preference(context, spFileName).getInt(key, alternative);
-        return integer;
+        return preference(context, spFileName).getInt(key, alternative);
     }
 
     public static boolean readBoolean(Context context, String spFileName, String key, boolean alternative) {
-        final boolean bln = preference(context, spFileName).getBoolean(key, alternative);
-        return bln;
+        return preference(context, spFileName).getBoolean(key, alternative);
     }
 
-    public static void writeString(Context context, String spFileName, String key, String text) {
+    public static void writeString(Context context, String spFileName, String key, String str) {
         SharedPreferences.Editor editor = preference(context, spFileName).edit();
-        editor.putString(key, text);
+        editor.putString(key, str);
+        editor.apply();
+    }
+
+    public static void writeFloat(Context context, String spFileName, String key, float flt) {
+        SharedPreferences.Editor editor = preference(context, spFileName).edit();
+        editor.putFloat(key , flt);
+        editor.apply();
+    }
+
+    public static void writeLong(Context context, String spFileName, String key, long lng) {
+        SharedPreferences.Editor editor = preference(context, spFileName).edit();
+        editor.putLong(key,lng);
         editor.apply();
     }
 
